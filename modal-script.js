049587d47a1d24ceee45ba614a02d77985cdc17c
Radina Yotova <44052854+GradinaYotova@users.noreply.github.com>
2022-02-24 10:12:05 +0100

    git-new

--- modal-script.js
+++ modal-script.js
@@ -1,18 +1,18 @@
 $( "#indexImg" ).click(function() {
-	$('.popuplayer').css('z-index', '5');
 	$('.wrapper').css('overflow', 'hidden');
+	$('.popuplayer').css('z-index', '5');
 	$('#myIndex').css('display', 'block');			
 });
 
 $( ".close" ).click(function() {
-	$('.popuplayer').css('z-index', '10');
 	$('.wrapper').css('overflow', 'visible');
+	$('.popuplayer').css('z-index', '10');
 	$('.myIndex').css('display', 'none');			
 });
 
 
 $( ".index-title" ).click(function() {
-	$('.popuplayer').css('z-index', '10');
 	$('.wrapper').css('overflow', 'visible');
+	$('.popuplayer').css('z-index', '10');
 	$(".myIndex").hide();
 });
