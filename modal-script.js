3ad883e37c97e8d3e80ce8511dba5981dee1d40a
Radina Yotova <44052854+GradinaYotova@users.noreply.github.com>
2022-03-15 01:01:17 +0100

    push-man

--- modal-script.js
+++ modal-script.js
@@ -1,18 +1,18 @@
 $( "#indexImg" ).click(function() {
 	$('.popuplayer').css('z-index', '5');
 	$('#myIndex').css('display', 'block');
-	$('body').css('position', 'fixed');			
+	$('.myBody').css('position', 'fixed');			
 });
 
 $( ".close" ).click(function() {
 	$('.popuplayer').css('z-index', '10');
 	$('.myIndex').css('display', 'none');
-	('body').css('position', 'relative');		
+	$('.myBody').css('position', 'relative');		
 });
 
 
 $( ".index-title" ).click(function() {
 	$('.popuplayer').css('z-index', '10');
 	$(".myIndex").hide();
-	('body').css('position', 'relative');
+	$('.myBody').css('position', 'relative');
 });
