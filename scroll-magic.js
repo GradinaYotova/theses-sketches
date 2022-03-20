5977071bdf4cff39e8b2205bde6332c1210eb06a
Radina Yotova <44052854+GradinaYotova@users.noreply.github.com>
2022-03-20 16:35:56 +0100

    pushe-news-

--- scroll-magic.js
+++ scroll-magic.js
@@ -38,12 +38,31 @@ var controller = new ScrollMagic.Controller();
 			.setClassToggle("#reveal4", "visible") // add class to reveal
 			.addTo(controller);
 
+			new ScrollMagic.Scene({
+			triggerElement: "#reveal5",
+			triggerHook: 0.4, // show, when scrolled 10% into view
+			duration: "100%", // hide 10% before exiting view (80% + 10% from bottom)
+			offset: 10 // move trigger to center of element
+			})
+			.setClassToggle("#reveal5", "visible") // add class to reveal
+			.addTo(controller);
+
+			new ScrollMagic.Scene({
+			triggerElement: "#reveal6",
+			triggerHook: 0.4, // show, when scrolled 10% into view
+			duration: "100%", // hide 10% before exiting view (80% + 10% from bottom)
+			offset: 10 // move trigger to center of element
+			})
+			.setClassToggle("#reveal6", "visible") // add class to reveal
+			.addTo(controller);
 
 
 var popup = $('#reveal1');
 var popuptwo = $('#reveal2');
 var popupthree = $('#reveal3');
 var popupfour = $('#reveal4');
+var popupfive = $('#reveal5');
+var popupsix = $('#reveal6');
 
 
 
@@ -60,9 +79,17 @@ var popupfour = $('#reveal4');
 	});
 
 	$( '.crossfour' ).click(function() {
-		popupthree.css('display', 'none');
+		popupfour.css('display', 'none');
 	});
 
+	$( '.crossfive' ).click(function() {
+		popupfive.css('display', 'none');
+	});
+
+
+	$( '.crosssix' ).click(function() {
+		popupsix.css('display', 'none');
+	});
 
 
 
